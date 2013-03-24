1cbcb72d06d23b0d51abfa5208a1dfaaa81e5a2c
anhminh115 <anh_minh11590@yahoo.com>
2013-03-24 21:46:32 +0700

    Initial Commit

---
+++ Ex02/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ex02
+//
+//  Created by Anh Minh on 3/24/13.
+//  Copyright (c) 2013 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ex02/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Ex02
+//
+//  Created by Anh Minh on 3/24/13.
+//  Copyright (c) 2013 FPT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
